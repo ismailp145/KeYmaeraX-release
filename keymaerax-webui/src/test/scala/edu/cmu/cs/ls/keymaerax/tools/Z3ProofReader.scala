d4c99cb823aea1237f3b5c1baa32caffe132ada9
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-10-17 11:03:24 -0500

    In Progress Var Matching

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/tools/Z3ProofReader.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/tools/Z3ProofReader.scala
@@ -27,12 +27,11 @@ import edu.cmu.cs.ls.keymaerax.tools.qe.SMTConverter
 import smtlib.trees.CommandsResponses.GetProofResponseSuccess
 import javax.swing.plaf.synth.SynthListUI
 import edu.cmu.cs.ls.keymaerax.btactics.DerivationInfoRegistry.convert
+import edu.cmu.cs.ls.keymaerax.tools.qe.DefaultSMTConverter._
 
 /** Reads [[Proofs]]s from SMT-LIB format: converts every (assert X) statement into an expression. */
 object Z3ProofReader {
   val USCORE: String = "uscore"
-  var lemmaMap: Map[String, ProvableSig] = Map()
-  var nameMap: Map[String, Formula] = Map()
 
   /** Reads a formula. */
   def readProof(s: String): ProvableSig = readExpr(s, convertProof(_)(Map.empty, Map.empty))
@@ -50,7 +49,7 @@ object Z3ProofReader {
   private def sanitize(name: String): String = { name.replace("_", USCORE) }
 
   /** Converts an SExpression. */
-  def convertProof(t: SExpr)(defs: Map[String, Expression], lemma: Map[String, ProvableSig]): ProvableSig = t match {
+  def convertProof(t: SExpr)(defs: Map[String, Formula], lemma: Map[String, ProvableSig]): ProvableSig = t match {
 
     case SList(SList(SSymbol("proof") :: steps :: Nil) :: Nil) => {
       println("Entered Proof")
@@ -61,28 +60,36 @@ object Z3ProofReader {
       println(s"Entered Let: $vars")
 
       vars.head match {
-        case SList(SSymbol(x) :: _) if (x.startsWith("$")) => {
-          println(s"Extracted: $x")
 
-          // nameMap = nameMap +
-          //   (x ->
-          //     convertSExprToFormula(
-          //       vars.head
-          //     )) // want to convert the rest of the sexpr to a formula so that it can be mapped
+        case SList(SSymbol(x) :: y :: Nil) if (x.startsWith("$")) => {
+          println(s"Extracted Variable: $x , $y")
 
+          var nameMap = defs +
+            (x ->
+              convertSExprToFormula(
+                y,
+                defs,
+              )) // want to convert the rest of the sexpr to a formula so that it can be mapped
+          convertProof(rest)(nameMap, lemma)
         }
-        case SList(SSymbol(x) :: _) if (x.startsWith("@")) => {
-          println(s"Extracted: $x")
-          lemmaMap = lemmaMap +
+
+        case SList(SSymbol(x) :: y) if (x.startsWith("@")) => {
+          println(s"Extracted Lemma : $x , $y")
+
+          var lemmaMap = lemma +
             (x -> convertProof(vars.head)(defs, lemma)) // mapping string lemma to provable sig on vars.head
           println(lemmaMap)
+          convertProof(rest)(defs, lemmaMap)
+
         }
 
-        case _ => println("No match found")
+        case _ =>
+          println("No match found")
+          convertProof(rest)(defs, lemma)
       }
 
-      convertProof(rest)(defs, lemma)
     }
+    // case SList(SSymbol("asserted") :: remainder) => { ??? }
 
     case SList(SSymbol(name) :: remainder) => {
       println(s"Entered SSymbol Name 1: $name, $remainder")
@@ -94,28 +101,33 @@ object Z3ProofReader {
       convertProof(remainder)(defs, lemma)
     }
 
-    case SSymbol(name) if name.startsWith("$") => {
-      // TO DO NEED TO FIX
-      print(s"This is name: $name")
-      // nameMap(name) // so that it can return formula but need to convert to provable sig
-      ???
-    }
-    case SSymbol(name) if name.startsWith("@") => lemmaMap(name) // so that it can return provable sig
-
     case GetProofResponseSuccess(steps) => { convertProof(steps)(defs, lemma) }
 
-    case _ => { throw new MatchError(t) }
+    // case SSymbol(name) if name.startsWith("$") => {
+    //   // TO DO NEED TO FIX
+    //   print(s"This is name: $name")
+    // }
+    // nameMap(name) // so that it can return formula but need to convert to provable sig
+    // ???
+    // }
+    // case SSymbol(name) if name.startsWith("@") => lemmaMap(name) // so that it can return provable sig
+    case y => {
+      println(y)
+      throw new MatchError(t)
+    }
   }
 
   /** Converts SExpression to Formula */
-  def convertSExprToFormula(sexpr: SExpr): Formula = sexpr match {
+  def convertSExprToFormula(sexpr: SExpr, defs: Map[String, Formula]): Formula = sexpr match {
 
-    case SSymbol(symbol) => PredOf(Function(symbol, None, Real, Bool), Nothing)
-    case SList(SSymbol("not") :: arg :: Nil) => Not(convertSExprToFormula(arg))
-    case SList(SSymbol("and") :: args) => args.map(convertSExprToFormula).reduceLeft(And.apply)
-    case SList(SSymbol("or") :: args) => args.map(convertSExprToFormula).reduceLeft(Or.apply)
+    case SSymbol(symbol) if symbol.startsWith("$") => defs(symbol)
+    case SSymbol(symbol) => PredOf(nameFromIdentifier(symbol).asInstanceOf[Function], Nothing)
+
+    case SList(SSymbol("not") :: arg :: Nil) => Not(convertSExprToFormula(arg, defs))
+    case SList(SSymbol("and") :: args) => args.map(convertSExprToFormula(_, defs)).reduceLeft(And.apply)
+    case SList(SSymbol("or") :: args) => args.map(convertSExprToFormula(_, defs)).reduceLeft(Or.apply)
     case SList(SSymbol("=>") :: left :: right :: Nil) =>
-      Imply(convertSExprToFormula(left), convertSExprToFormula(right))
+      Imply(convertSExprToFormula(left, defs), convertSExprToFormula(right, defs))
     case SList(SSymbol("=") :: left :: right :: Nil) => Equal(convertSExprToTerm(left), convertSExprToTerm(right))
     case _ => throw new IllegalArgumentException(s"Unsupported SExpr: $sexpr")
     // had to create an sexpr to term method because Equal takes in terms
